ecccef44bb203c6cc4d1e2524c3a4f0040ceed62
Urraca <h.urraca.g574@edu.utc.mx>
2026-01-30 09:40:04 -0600

    Fix EJS engine and Render port

--- index.js
+++ index.js
@@ -1,54 +1,28 @@
 const express = require('express');
-const app = express();
 const path = require('path');
 
-// motor de vistas
+const app = express();
+
+// EJS
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
 
-// archivos estáticos
+// estáticos
 app.use(express.static(path.join(__dirname, 'public')));
 
-// ruta principal
-app.get('/mercurio', (req, res) => {
-    res.render('pages/mercurio');
-    console.log('pagina planeta cargada');
-});
-app.get('/venus', (req, res) => {
-    res.render('pages/venus');
-    console.log('pagina planeta cargada');
-});
-app.get('/tierra', (req, res) => {
-    res.render('pages/tierra');
-    console.log('pagina planeta cargada');
-});
-app.get('/marte', (req, res) => {
-    res.render('pages/marte');
-    console.log('pagina planeta cargada');
-});
-app.get('/jupiter', (req, res) => {
-    res.render('pages/jupiter');
-    console.log('pagina planeta cargada');
-});
-app.get('/saturno', (req, res) => {
-    res.render('pages/saturno');
-    console.log('pagina planeta cargada');
-});
-app.get('/urano', (req, res) => {
-    res.render('pages/urano');
-    console.log('pagina planeta cargada');
-});
-app.get('/neptuno', (req, res) => {
-    res.render('pages/neptuno');
-    console.log('pagina planeta cargada');
-});
-app.get('/', (req, res) => {
-    res.render('pages/planetas');
-    console.log('pagina planeta cargada');
-});
+// rutas
+app.get('/', (req, res) => res.render('pages/planetas'));
+app.get('/mercurio', (req, res) => res.render('pages/mercurio'));
+app.get('/venus', (req, res) => res.render('pages/venus'));
+app.get('/tierra', (req, res) => res.render('pages/tierra'));
+app.get('/marte', (req, res) => res.render('pages/marte'));
+app.get('/jupiter', (req, res) => res.render('pages/jupiter'));
+app.get('/saturno', (req, res) => res.render('pages/saturno'));
+app.get('/urano', (req, res) => res.render('pages/urano'));
+app.get('/neptuno', (req, res) => res.render('pages/neptuno'));
 
+// puerto Render
 const PORT = process.env.PORT || 4000;
-
 app.listen(PORT, () => {
   console.log('Servidor corriendo en puerto', PORT);
 });
